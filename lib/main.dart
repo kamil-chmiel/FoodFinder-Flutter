d8efa24394c4e87594990e5872223f962858304b
Kamil Chmiel <kamil.chmiel@ready4s.com>
2020-02-01 15:21:58 +0100

    Added ad section

--- lib/main.dart
+++ lib/main.dart
@@ -116,13 +116,48 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   GestureDetector(
                       child: Padding(
-                        padding: const EdgeInsets.only(left: 10.0),
-                        child: Icon(Icons.developer_board,
-                            color: Colors.greenAccent),
-                      )),
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child:
+                        Icon(Icons.developer_board, color: Colors.greenAccent),
+                  )),
                 ],
               ),
             ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.greenAccent,
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Image(
+                    image: AssetImage('assets/images/burgerBox.png'),
+                    fit: BoxFit.fill,
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 37, left: 27),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'NEW!',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 25),
+                          ),
+                          Text(
+                            '20 new In-n-Out places',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18),
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+            )
           ],
         ),
       ),
